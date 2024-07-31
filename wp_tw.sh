e3fe9a364ffaa6fc5ad79e6e311cd7eaa6d74ae7
Pablo Sazo <pablo@glooadvertising.com.au>
2024-07-31 14:35:52 +1000

    _tw

--- wp_tw.sh
+++ wp_tw.sh
@@ -10,8 +10,6 @@ wp core download --path=$PROJECT_NAME
 
 cd $PROJECT_NAME
 
-valet link
-
 wp config create --dbname=$PROJECT_NAME --dbuser=root --dbprefix="gloo_"
 
 # Set the WP_DEBUG constant to true.
@@ -19,20 +17,36 @@ wp config set WP_DEBUG true --raw
 
 wp db create
 
+
 echo "Please provide a title for your new WordPress site."
 read SITE_TITLE
 
+
 wp core install --url=http://$PROJECT_NAME.test --title="$SITE_TITLE" --admin_user="gloo" --admin_password="advertisingthatsticks" --admin_email=websites@glooadvertising.com.au --locale=en_AU
 
-wp post create --post_type=page --post_title='Home' --post_status=publish
 wp plugin delete akismet
 wp plugin delete hello
+
 wp post delete 1 2 3
+# wp post delete --post_type=page --post_title='Sample Page'
+# wp post delete --post_type=page --post_title='Privacy Policy'
+
+wp post create --post_type=page --post_title='Home' --post_status=publish
+
+valet link
 
 cd wp-content/themes/
+
 git clone https://github.com/jeffreyvr/tailpress.git
-mv tailpress $PROJECT_NAME-gloo-theme
-cd $PROJECT_NAME-gloo-theme && rm -rf .git && code . && wp theme activate $PROJECT_NAME-gloo-theme
+
+mv tailpress $PROJECT_NAME-theme
+
+wp theme activate $PROJECT_NAME-theme
+
+cd $PROJECT_NAME-theme
+
+code .
+
 open http://$PROJECT_NAME.test
 
-# mkdir ./wp-content/themes/$PROJECT_NAME-gloo-theme && code ./wp-content/themes/$PROJECT_NAME-gloo-theme
+# mkdir ./wp-content/themes/$PROJECT_NAME-theme && code ./wp-content/themes/$PROJECT_NAME-theme
---
+++ yt-dlp.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+cd ~/Movies
+
+NEWDIR=ytdlp_dwnlds
+
+if [[ ! -e $NEWDIR ]]; then
+    mkdir $NEWDIR
+fi
+
+cd $NEWDIR
+
+echo "Hello! Please provide the URL of the YouTube video you want us to download."
+read -p "YouTube video URL: " YT_URL
+
+yt-dlp -f bestvideo+bestaudio/best --merge-output-format mkv $YT_URL
